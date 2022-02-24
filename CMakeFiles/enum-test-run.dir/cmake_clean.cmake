6d28513facfc59f06073df28de8f71138e8d3832
yamaceay <yamacerenay2001@gmail.com>
2022-02-24 09:26:27 +0100

    gitignore

--- CMakeFiles/enum-test-run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/enum-test-run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/enum-test-run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CMakeFiles/flag-test-run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/flag-test-run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/flag-test-run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
